d1790752ea315e6fc4b84e5ca323c24d13b36d64
marcos.blandim <marcos.blandim@seatecnologia.com.br>
2021-04-14 00:04:36 -0300

    feat: style Game

--- sueca/src/components/Game/index.js
+++ sueca/src/components/Game/index.js
@@ -1,10 +1,13 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import getFullDeck from '../../utils/getFullDeck';
-import './styles.css';
+
+import './styles.scss';
 
 function Game({ selectedRule }) {
+  const backCardFilename = 'XX';
+
   const [cards, setCards] = useState(getFullDeck());
   const [keepCurrCard, setKeepCurrCard] = useState(false);
   const [currCardIndex, setCurrCardIndex] = useState(null);
@@ -34,70 +37,125 @@ function Game({ selectedRule }) {
   };
 
   const resetDeck = () => {
-    setCards(getFullDeck());
-    setDiscardPile([]);
-    setCurrCardIndex(null);
-  };
-
-  const getCurrCard = () => {
-    return cards[currCardIndex];
-  };
-
-  const ConditionalCard = () => {
-    if (!cards.length) {
-      return <p>Deck is empty</p>;
-    } else if (currCardIndex != null) {
-      return <Card />;
-    } else {
-      return <p>No card drawn</p>;
+    if (confirm('Deseja reiniciar o baralho?')) {
+      setCards(getFullDeck());
+      setDiscardPile([]);
+      setCurrCardIndex(null);
     }
   };
 
-  const Card = () => {
-    const cardKey = getCurrCard()[0];
-    const {
-      name: ruleName,
-      description: ruleDescription,
-    } = selectedRule.content[cardKey];
+  const getCurrCard = () => {
     return (
-      <React.Fragment>
-        <p>
-          <img
-            alt={`${getCurrCard()} card`}
-            className='card'
-            // src={`./cards/${getCurrCard()}.png`}
-            src={`https://deckofcardsapi.com/static/img/${getCurrCard()}.png`}
-          />
-        </p>
-        <h3 style={{ marginLeft: '40px' }}>{ruleName}</h3>
-        <p style={{ marginLeft: '40px' }}>{ruleDescription}</p>
-      </React.Fragment>
+      currCardIndex !== null &&
+      (cards.length || undefined) &&
+      cards[currCardIndex]
     );
   };
 
+  const isDeckFull = () => {
+    return cards.length === getFullDeck().length && currCardIndex === null;
+  };
+
+  const isDeckEmpty = () => {
+    return cards.length === 0;
+  };
+
+  const currCard = getCurrCard() || backCardFilename;
+
+  const cardKey =
+    currCard !== backCardFilename ? currCard[0] : backCardFilename;
+
+  const deckIsEmpty = isDeckEmpty();
+  const deckIsFull = isDeckFull();
+
+  const imgAlt = !(deckIsEmpty || deckIsFull)
+    ? `${currCard} card`
+    : deckIsEmpty
+    ? 'Baralho vazio 🕸️'
+    : 'Nenhuma carta  🃏';
+
+  const ruleName = !(deckIsEmpty || deckIsFull)
+    ? selectedRule.content[cardKey].name
+    : deckIsEmpty
+    ? 'Baralho vazio 🕸️'
+    : 'Nenhuma carta  🃏';
+
+  const ruleDescription = !(deckIsEmpty || deckIsFull)
+    ? selectedRule.content[cardKey].description
+    : deckIsEmpty
+    ? 'Reinicie o baralho'
+    : 'Compre a próxima carta';
+
+  const KeepCurrCardSwitch = () => (
+    <div className='custom-control custom-switch'>
+      <input
+        type='checkbox'
+        className='custom-control-input'
+        id='customSwitch1'
+        onChange={toggleKeepCurrCard}
+        checked={keepCurrCard}
+      />
+      <label className='custom-control-label ' htmlFor='customSwitch1'>
+        Manter cartas
+      </label>
+    </div>
+  );
+
   return (
-    <React.Fragment>
-      <h1>Game</h1>
-      <p>
-        <Link to='/'>{'<'} Back</Link>
-      </p>
-      <p>Keep card: {keepCurrCard ? 'true' : 'false'}</p>
-      <p>Number of cards: {cards.length}</p>
+    <div className='container mt-4 panel p-3 w-50'>
+      <div className='row'>
+        <div className='offset-2 col'>
+          <Link to='/'>{'<'} Back</Link>
+        </div>
+        <div className='offset-1 col'>
+          <p>Cartas restantes: {cards.length}</p>
+        </div>
+      </div>
 
-      <button type='button' onClick={toggleKeepCurrCard}>
-        Toggle keep card
-      </button>
-      <br />
-      <button type='button' onClick={resetDeck}>
-        Reset deck
-      </button>
-      <br />
-      <button type='button' onClick={getRandomCard}>
-        Next card
-      </button>
+      <div className='row justify-content-center'>
+        <div className='mb-4 mt-2'>
+          <button
+            className='next-card-btn btn btn-primary'
+            onClick={getRandomCard}
+            disabled={isDeckEmpty()}
+          >
+            Próxima carta
+          </button>
+        </div>
+      </div>
 
-      <ConditionalCard />
-    </React.Fragment>
+      <div className='row '>
+        <div className='offset-2 col-4'>
+          <img
+            alt={imgAlt}
+            className='playing-card'
+            src={`./cards/${currCard}.png`}
+          />
+        </div>
+        <div className='col-6'>
+          <h3>{ruleName}</h3>
+          <p>{ruleDescription}</p>
+        </div>
+      </div>
+
+      <div className='row justify-content-center'>
+        <div className='m-4'>
+          <button
+            className='next-card-btn btn btn-secondary'
+            onClick={resetDeck}
+            disabled={isDeckFull()}
+          >
+            Reiniciar baralho
+          </button>
+        </div>
+      </div>
+
+      <div className='row justify-content-center'>
+        <div className='mb-2'>
+          <KeepCurrCardSwitch onClick={toggleKeepCurrCard} />
+        </div>
+      </div>
+    </div>
   );
 }
 
